d6360c34f5795361d5d79add8e5b92e963756e9a
RkGames <39560269+RkGames@users.noreply.github.com>
2020-05-12 20:03:51 +0200

    :P

---
+++ 3DLevelDesign/Assets/debugModeTeleport.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class debugModeTeleport : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform initPos;
+    public Transform checkpoint1;
+    public Transform checkpoint2;
+    public Transform checkpoint3;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            transform.position = initPos.position;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            transform.position = checkpoint1.position;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            transform.position = checkpoint2.position;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            transform.position = checkpoint3.position;
+        }
+    }
+}
---
+++ 3DLevelDesign/Assets/timeManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class timeManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    bool isTimeSlowed = false;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            if (!isTimeSlowed)
+            {
+                Time.timeScale = 0;
+                isTimeSlowed = true;
+            }
+            else
+            {
+                Time.timeScale = 1;
+                isTimeSlowed = false;
+            }
+        }
+    }
+}
